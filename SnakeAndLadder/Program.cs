5191372b6caf96e7c1761190b2175c3299e354fa
vaibhav <vaibhavmehta524@gmail.com>
2022-12-23 22:47:50 +0530

    Single player snake and ladder

---
+++ SnakeAndLadder/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+
+namespace SnakeAndLadderProblems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string Player = "Rahul";
+            int position = 0;
+            Console.WriteLine("Welcome to the snake and ladder Simulator");
+
+            //UC1 For single player
+            Console.WriteLine("Single player name is player:{0} and its position:{1}: ", Player, position);
+            Console.ReadLine();
+        }
+    }
+}
