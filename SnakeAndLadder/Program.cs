84994d0bb13e423afc7ee130415440269033ded0
vaibhav <vaibhavmehta524@gmail.com>
2022-12-23 23:24:56 +0530

    Play the game with 2 Player. In this case if a Player gets a Ladder then plays again. Finally report which Player won the game

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -8,13 +8,16 @@ namespace SnakeAndLadderProblems
     {
         static void Main(string[] args)
         {
-            string Player = "Rahul";
-            int position = 0, Option = 0, roll = 0, Count = 0;
+            string Player1 = "Rahul";
+            string Player2 = "Karan";
+            int position = 0, Option = 0, roll = 0, Count = 0, position1 = 0;
+            bool turn = true;
 
             Console.WriteLine("Welcome to the snake and ladder Simulator");
 
             //UC1 For single player
-            Console.WriteLine("Single player name is player:{0} and its position:{1}", Player, position);
+            Console.WriteLine("Player1 name is player1:{0} and its position:{1}", Player1, position);
+            Console.WriteLine("Player2 name is player2:{0} and its position:{1}", Player2, position1);
 
             //UC2 player roll die get number between 1 to 6
             //Random random = new Random();
@@ -22,60 +25,123 @@ namespace SnakeAndLadderProblems
             //Console.WriteLine("The single player rolled die possibility is: "+roll);
 
             //UC3 No Play,Ladder or Snake
-            //Random random1= new Random();
-            //Option = random1.Next(1 , 4);
 
             //UC4 repeating until player gets wining position
-            while (position < 100)
+            while (position < 100 && position1 < 100)
             {
                 Random random = new Random();
                 roll = random.Next(1, 7);
                 Console.WriteLine("die value" + roll);
+
                 Random random1 = new Random();
                 Option = random1.Next(1, 4);
-
-
-                switch (Option)
+                if (turn == true)
                 {
-                    case 1:
-                        Console.WriteLine("(No Play)");
-                        position = position + 0;
-                        Console.WriteLine("player position same " + position);
-                        //UC6 Counting the number of times the die rolled 
-                        Count = Count + 1;
-                        break;
-                    case 2:
-                        Console.WriteLine(" Get A ladder");
-                        position += roll;  //position+roll
-                        //UC5 if player gets more than 100th position then position will be reduced
-                        if (position > 100)
+                    if (position < 100)
+                    {
+                        switch (Option)///player one
                         {
-                            position = position - roll;
-                            Console.WriteLine("If player reach more than 100th position then position will be reduced" + roll);
+                            case 1:
+                                Console.WriteLine("(No Play)");
+                                position = position + 0;
+                                turn = false;
+                                Console.WriteLine("player position same " + position);
+                                //UC6 Counting the number of times the die rolled 
+                                Count = Count + 1;
+                                break;
+                            case 2:
+                                Console.WriteLine(" Get A ladder");
+                                position += roll;  //position+roll
+                                //UC5 if player gets more than 100th position then position will be reduced
+                                if (position > 100)
+                                {
+                                    position = position - roll;
+                                    Console.WriteLine("If player reach more than 100th position then position will be reduced" + roll);
+                                }
+                                else
+                                {
+                                    Console.WriteLine("After getting ladder player position will be :" + position);
+                                }
+                                turn = true;
+                                Count = Count + 1;
+                                break;
+                            case 3:
+                                Console.WriteLine(" Get a Snake bite");
+                                position = position - roll;
+                                if (position <= 0)
+                                {
+                                    position = 0;
+                                }
+                                else
+                                {
+                                    Console.WriteLine("After getting snake bite player position will be:" + position);
+                                }
+                                turn = false;
+                                Count = Count + 1;
+                                break;
                         }
-                        else
-                        {
-                            Console.WriteLine("After getting ladder player position will be :" + position);
-                        }
-                        Count = Count + 1;
-                        break;
-                    case 3:
-                        Console.WriteLine(" Get a Snake bite");
-                        position = position - roll;
-                        if (position <= 0)
-                        {
-                            position = 0;
-                        }
-                        else
-                        {
-                            Console.WriteLine("After getting snake bite player position will be:" + position);
-                        }
-                        Count = Count + 1;
-                        break;
+                        Console.WriteLine("Player1 is won the game");
+                    }
                 }
-
+                else
+                {
+                    if (position1 < 100)
+                    {
+                        switch (Option)//player two
+                        {
+                            case 1:
+                                Console.WriteLine("(No Play)");
+                                position1 = position1 + 0;
+                                Console.WriteLine("player2 position1 same " + position1);
+                                turn = true;
+                                //UC6 Counting the number of times the die rolled 
+                                Count = Count + 1;
+                                break;
+                            case 2:
+                                Console.WriteLine(" Get A ladder");
+                                position1 += roll;  //position+roll
+                                                    //UC5 if player gets more than 100th position then position will be reduced
+                                if (position1 > 100)
+                                {
+                                    position1 = position1 - roll;
+                                    Console.WriteLine("If player2 reach more than 100th position then position will be reduced" + roll);
+                                }
+                                else
+                                {
+                                    Console.WriteLine("After getting ladder player2 position will be :" + position1);
+                                }
+                                turn = false;
+                                Count = Count + 1;
+                                break;
+                            case 3:
+                                Console.WriteLine(" Get a Snake bite");
+                                position1 = position1 - roll;
+                                if (position1 <= 0)
+                                {
+                                    position1 = 0;
+                                }
+                                else
+                                {
+                                    Console.WriteLine("After getting snake bite player2 position will be:" + position1);
+                                }
+                                turn = true;
+                                Count = Count + 1;
+                                break;
+                        }
+                        Console.WriteLine("Player2 is won the game");
+                    }
+                }
             }
-            Console.WriteLine("The players max position reached:" + position);
+            Console.WriteLine("The player1 max position reached:" + position);
+            Console.WriteLine("The player2 max position1 reached:" + position1);
+            if (position > position1)
+            {
+                Console.WriteLine("Player1 win the game");
+            }
+            else
+            {
+                Console.WriteLine("Player2 win the game");
+            }
             Console.WriteLine("count" + Count);
             Console.ReadLine();
         }
