4b21d1e6bc4a0bcecd60f93c4708457f03c21b92
vaibhav <vaibhavmehta524@gmail.com>
2022-12-23 23:07:36 +0530

    Repeat till player reaches wining position 100

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -9,7 +9,7 @@ namespace SnakeAndLadderProblems
         static void Main(string[] args)
         {
             string Player = "Rahul";
-            int position = 0, Option = 0;
+            int position = 0, Option = 0, roll = 0;
 
             Console.WriteLine("Welcome to the snake and ladder Simulator");
 
@@ -17,29 +17,60 @@ namespace SnakeAndLadderProblems
             Console.WriteLine("Single player name is player:{0} and its position:{1}", Player, position);
 
             //UC2 player roll die get number between 1 to 6
-            Random random = new Random();
-            int roll = random.Next(1, 7);
-            Console.WriteLine("The single player rolled die possibility is: " + roll);
+            //Random random = new Random();
+            //roll = random.Next(1 , 7);
+            //Console.WriteLine("The single player rolled die possibility is: "+roll);
 
             //UC3 No Play,Ladder or Snake
-            Random random1 = new Random();
-            Option = random1.Next(0, 3);
-            switch (Option)
+            //Random random1= new Random();
+            //Option = random1.Next(1 , 4);
+
+            //UC4 repeating until player gets wining position
+            while (position < 100)
             {
-                case 1:
-                    Console.WriteLine("Player is at same position(No Play)");
-                    break;
-                case 2:
-                    Console.WriteLine("Player gets a ladder");
-                    position += roll;  //position+roll 
-                    Console.WriteLine("After getting ladder player position will be :" + position);
-                    break;
-                default:
-                    Console.WriteLine("Player will get Snake bite and position will decrease");
-                    position = roll - position;
-                    Console.WriteLine("After getting snake bite player position will be:" + position);
-                    break;
+                Random random = new Random();
+                roll = random.Next(1, 7);
+                Console.WriteLine("die value" + roll);
+                Random random1 = new Random();
+                Option = random1.Next(1, 4);
+
+
+                switch (Option)
+                {
+
+                    case 1:
+                        Console.WriteLine("(No Play)");
+                        position = position + 0;
+                        Console.WriteLine("player position same " + position);
+                        break;
+                    case 2:
+                        Console.WriteLine(" a ladder");
+                        position += roll;  //position+roll
+                        if (position > 100)
+                        {
+                            position = position - roll;
+                            Console.WriteLine("If player reach more than 100th position then position will be reduced" + roll);
+                        }
+                        else
+                        {
+                            Console.WriteLine("After getting ladder player position will be :" + position);
+                        }
+                        break;
+                    case 3:
+                        Console.WriteLine(" get Snake bite");
+                        position = position - roll;
+                        if (position <= 0)
+                        {
+                            position = 0;
+                        }
+                        else
+                        {
+                            Console.WriteLine("After getting snake bite player position will be:" + position);
+                        }
+                        break;
+                }
             }
+            Console.WriteLine("The players max position reached:" + position);
             Console.ReadLine();
         }
     }
