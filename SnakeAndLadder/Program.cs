8f6498baa7eb8ff09bf3efda61d5eacc0bd598d0
vaibhav <vaibhavmehta524@gmail.com>
2022-12-23 22:53:08 +0530

    Player rolls dice

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -13,6 +13,11 @@ namespace SnakeAndLadderProblems
 
             //UC1 For single player
             Console.WriteLine("Single player name is player:{0} and its position:{1}: ", Player, position);
+
+            //UC2 player roll die get number between 1 to 6
+            Random random = new Random();
+            int roll = random.Next(1, 7);
+            Console.WriteLine("The single player rolled die possibility is: " + roll);
             Console.ReadLine();
         }
     }
